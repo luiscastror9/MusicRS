22d32ddb17300365cc77c622eaa8cbf2b319b500
Matias <matias.gomez@outlook.com>
2018-03-26 20:00:59 -0300

    Cambios en About

--- BandasWeb/BandasWeb/App_Start/BundleConfig.cs
+++ BandasWeb/BandasWeb/App_Start/BundleConfig.cs
@@ -26,7 +26,8 @@ namespace BandasWeb
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/About.css"));
         }
     }
 }
--- BandasWeb/BandasWeb/Controllers/HomeController.cs
+++ BandasWeb/BandasWeb/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace BandasWeb.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Acerca de nosotros";
 
             return View();
         }
